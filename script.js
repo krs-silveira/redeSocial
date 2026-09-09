13456df33e833b3692181e8a91f5063b449350b9
krs-silveira <liviazanonasilveira@gmail.com>
2026-09-09 11:47:49 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
     const likeBtn = document.querySelector(".like-btn");
+const postMedia = document.queryselector(".post-media");
+if (!likeBtn) return;
+
+const likesCountSpan = likeBtn.querySelector(".likes-count");
+const boomarkBtn = document.querySelector(".bookmark-btn");
+
+let isLiked = false;
+let baseLikes = 0;
+
+//texto inicial
+
+if(likesCountSpan){
+    likesCountSpan.textContent = "0";
+}
+
+//numeros acima de 1mil
+
+function formatLikes (num){
+    if (num >=1000){
+        return (num/1000).toFixed(1)+"K";
+    }
+    return num.toString();
+}
+
+
+
+
+
 })
